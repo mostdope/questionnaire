05f2cf1acd05038672c925a0ec0f6a2c0786a0bb
mostdope <mostdope@live.ru>
2015-10-06 08:37:38 +0300

    bug with untracked files, add controller and index

---
+++ QuestionnaireDemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using QuestionnaireDemo.Models;
+
+namespace QuestionnaireDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var list = new List<Question> {new Question("ЧЛЕН"), new Question("ДРУГОЙ ЧЛЕН")};
+
+
+            return View(list);
+        }
+    }
+}
---
+++ QuestionnaireDemo/Models/Answer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuestionnaireDemo.Models
+{
+    public class Answer
+    {
+        public Answer()
+        {
+            
+        }
+        public Answer(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
